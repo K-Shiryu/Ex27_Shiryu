1fd77a9722ccd40b133e993c6852c866e8104b9a
2U <edo-roi-hagaren@outlook.jp>
2021-12-08 10:03:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class Triangle
+    {
+    private float width;    //　幅
+    private float height;   //　高さ
+    private float[] sideLength = new float[3];
+    private float _surface;  // コンストラクタで代入
+    public float surface
+    {   // 外部から読み取りだけできる変数
+        get
+        {
+            return _surface;
+        }
+    }
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="w">幅</param>
+    /// <param name="h">高さ</param>
+    public Triangle(float w, float h)
+    {
+        this.width = w;
+        this.height = h;
+        _surface = width * height / 2;
+    }
+    public Triangle(float side1, float side2, float side3)
+    {
+        this.sideLength[0] = side1;
+        this.sideLength[1] = side2;
+        this.sideLength[2] = side3;
+        float s = (sideLength[0] + sideLength[1] + sideLength[2]) / 2;
+        _surface = MathF.Sqrt(
+s * (s - sideLength[0])
+* (s - sideLength[1])
+* (s - sideLength[2]));
+    }
+    /// <summary>
+    /// 面積を取得
+    /// </summary>
+    /// <returns>面積</returns>
+    public float GetSurface()
+    {
+        return _surface;
+    }
+}
